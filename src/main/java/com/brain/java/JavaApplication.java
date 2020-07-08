2375d8785a5c4901b7a82cd6708c1c71bcbc8e8f
lanjian <lanjian@mylike.com>
2020-07-08 18:42:12 +0800

    ！！！！

--- src/main/java/com/brain/java/JavaApplication.java
+++ src/main/java/com/brain/java/JavaApplication.java
@@ -2,12 +2,12 @@ package com.brain.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class JavaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(JavaApplication.class, args);
     }
 }
